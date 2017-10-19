2ec6c05d3701938d0767bef6f875ca5447446ba0
Kemanth PJ <kemanth.raj@gmail.com>
2017-10-19 17:42:06 +0530

    Updated serial_ants.cpp

--- serial_ants.cpp
+++ serial_ants.cpp
@@ -4,6 +4,7 @@
 #include<stdlib.h>
 
 #define MAX_CITIES 50
+#define MAX_ANTS 50
 
 using namespace std;
 
@@ -11,9 +12,17 @@ struct cities
 {
 	int x,y;
 };
+struct ants{
+	
+	int curCity, nextCity, pathIndex;
+	int visited[MAX_CITIES];
+	int path[MAX_CITIES];
+	double tourLength;
+};
 
 double pheromone[MAX_CITIES][MAX_CITIES];
 double dist[MAX_CITIES][MAX_CITIES];
+ants ant[MAX_ANTS];
 
 void initialize(cities city[],int n)
 {
@@ -30,6 +39,24 @@ void initialize(cities city[],int n)
 			}
 		}	
 	}
+	
+	int visit=0;
+	for(int i=0;i<MAX_ANTS;i++)
+	{
+		if(i==n)
+			visit=0;
+		ant[i].curCity=visit++;
+		for(int j=0;j<n;j++)
+		{
+			ant[i].visited[j]=0;
+			ant[i].path[j]=-1;
+		}
+		ant[i].pathIndex = 1;
+		ant[i].path[0] = ant[i].curCity;
+		ant[i].nextCity = -1;
+		ant[i].tourLength = 0;
+		ant[i].visited[ant[i].curCity]=1;
+	}
 }
 
 int main()
@@ -45,7 +72,9 @@ int main()
 		in>>city[i].x;
 		in>>city[i].y;	
 	}
+	//initialize the ants and place them on the TSP cities 
 	initialize(city,ncities);
+	
 	return 0;
 }
 
